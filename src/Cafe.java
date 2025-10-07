9a67ef5d53f37b7e2352104945630cda338fc1bd
killmyselff <juangaleano857@gmail.com>
2025-10-06 22:15:34 -0500

    Añadir clases Cafe y TiendaCafe para la actividad de POO

---
+++ src/Cafe.java
@@ -0,0 +1,30 @@
+package src;
+
+public class Cafe {
+    private String nombre;
+    private String region;
+    private double precioPorKilo;
+    private float cantidadEnKilos;
+
+    public Cafe(String nombre, String region, double precioPorKilo, float cantidadEnKilos) {
+        this.nombre = nombre;
+        this.region = region;
+        this.precioPorKilo = precioPorKilo;
+        this.cantidadEnKilos = cantidadEnKilos;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Región: " + region);
+        System.out.println("Precio por kilo: " + precioPorKilo + " COP");
+        System.out.println("Cantidad en kilos: " + cantidadEnKilos);
+    }
+
+    public double calcularPrecioTotal() {
+        return precioPorKilo * cantidadEnKilos;
+    }
+
+    public void actualizarCantidad(float nuevaCantidad) {
+        this.cantidadEnKilos = nuevaCantidad;
+    }
+}
---
+++ src/TiendaCafe.java
@@ -0,0 +1,25 @@
+package src;
+
+public class TiendaCafe {
+    public static void main(String[] args) {
+        Cafe cafe1 = new Cafe("Café de Nariño", "Nariño", 50000, 10);
+        Cafe cafe2 = new Cafe("Café de Antioquia", "Antioquia", 45000, 20);
+        Cafe cafe3 = new Cafe("Café del Huila", "Huila", 55000, 15);
+
+        System.out.println("Información de los cafés:");
+        System.out.println("------------------------");
+        cafe1.mostrarInformacion();
+        System.out.println("Precio total: " + cafe1.calcularPrecioTotal() + " COP");
+        System.out.println("------------------------");
+        cafe2.mostrarInformacion();
+        System.out.println("Precio total: " + cafe2.calcularPrecioTotal() + " COP");
+        System.out.println("------------------------");
+        cafe3.mostrarInformacion();
+        System.out.println("Precio total: " + cafe3.calcularPrecioTotal() + " COP");
+
+        System.out.println("\nActualizando la cantidad del café de Nariño...");
+        cafe1.actualizarCantidad(5);
+        System.out.println("------------------------");
+        cafe1.mostrarInformacion();
+    }
+}
